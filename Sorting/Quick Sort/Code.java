f27ba75d1cfc321a9ee497f1d0bf7e7b118ceee6
Mouri Roy <87095935+lognoroy@users.noreply.github.com>
2022-10-01 22:47:13 +0530

    Create Code.java

---
+++ Sorting/Quick Sort/Code.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+    	QuickSort(nums, 0, nums.length - 1);
+    	int result[] = new int[nums.length]; 
+        for (int i = 0; i < nums.length; i++) {
+        	result[i] = nums[i];
+        }
+        return result;
+    }
+    
+    public static void QuickSort(int nums[], int lhs, int rhs) {
+    	if(lhs >= rhs) return; 
+		int mid = Partition(nums, lhs, rhs);
+		QuickSort(nums, lhs, mid);
+		QuickSort(nums, mid + 1, rhs);
+    }
+    
+    public static void Swap(int[] nums, int lhs, int rhs) {
+    	int temp = nums[lhs];
+    	nums[lhs] = nums[rhs]; 
+    	nums[rhs] = temp;
+    }
+    
+    public static int Partition(int[] nums, int lhs, int rhs) {
+    	int pivot = nums[lhs];
+    	while(lhs < rhs) {
+            while (lhs < rhs && nums[rhs] >= pivot) rhs--;
+            Swap(nums, lhs, rhs);
+            while (lhs < rhs && nums[lhs] <= pivot) lhs++;
+            Swap(nums, rhs, lhs);
+    	}
+    	nums[lhs] = pivot;
+    	return lhs;
+    }
+}
