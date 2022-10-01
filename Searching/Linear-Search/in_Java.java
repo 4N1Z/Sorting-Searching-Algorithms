8fc812bf5ef625d97a42ee45e39dea3c426b24eb
Akash Dev <107734937+AkashDev23@users.noreply.github.com>
2022-10-02 05:15:03 +0530

    fixed typos and grammar
    I've added time and space complexity, fixed some typos and formated the code a little bit..

--- Searching/Linear-Search/in_Java.java
+++ Searching/Linear-Search/in_Java.java
@@ -9,33 +9,33 @@
 /*
  * 
  *  * LINEAR SEARCH -
-     * Start searching from the first element till you find the element in an aray
+     * Start searching from the first element till you find the element in an array
      * and return the index.
      * 
      * -if value doesnt exist then return -1.
      * -max comaprisons in the worst case will be 'N'(big-oh(N))
+     * the space complexity of linear search algorithm is big-oh(1) since searching doesn't requires any extra space. 
+     * the time complexity of linear search algorithm is big-oh(N) since in the worst case it will be doing big-oh(N) comparisons. 
  */
 
 
  public class in_Java{
  
     //Code starts from here
-    static void linearsearch(int arr[], int target) {
+    static void linearSearch(int arr[], int target) {
         int found = 0;
         int index = 0;
 
         for (int i = 0; i < arr.length; i++) {
             
-            if (target == arr[i]) {
+            if (target == arr[i]) {// if index is found 
                 found = 1;
-                index = i+1 ;
+                index = i+1;//increase the index number since array index starts with 0
             }
         }
         if (found == 0)
             System.out.println("Not found");
         else
             System.out.println("Found at position : " + index);
-    }
-
-    
+    }   
  }
