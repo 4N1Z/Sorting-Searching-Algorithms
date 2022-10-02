93d8e8e5267cf4b29614fdd65558c0a484b77574
ayusshrathore <ayusshrathore@gmail.com>
2022-10-02 10:40:48 +0530

    Add explanation & complexities for QuickSort

--- Sorting/QuickSort/QuickSort.java
+++ Sorting/QuickSort/QuickSort.java
@@ -1,3 +1,26 @@
+/**
+ * Explanation:
+ * 
+ * In this algorithm, we start by selecting the item which is known as a pivot,
+ * and then we re-arrange all the elements such that elements smaller than the
+ * pivot are on the left side & elements larger than the pivot appear on the
+ * right side of the pivot which is called partitioning, so we divide the array
+ * into two partitions that are separated by the pivot.
+ * 
+ * The same process is continued into the other two partitions, also in the case
+ * of reordering the items we did not worry about the right partition so we have
+ * narrowed down the scope of our work this is the reason why quick sort has
+ * logarithmic complexity because at every step we almost narrow down the size
+ * of our partition almost breaking them into half.
+ * 
+ * Space & Time Complexity:
+ * 
+ * Space -> O(log(n))
+ * 
+ * Time -> O(n log(n))
+ * 
+ */
+
 public class QuickSort {
   public static void sort(int[] arr) {
     sort(arr, 0, arr.length - 1);
