ed18b2eea85ebac7838ad0282c9f1bb4963d8162
Atharva-D <atharvadusane13@gmail.com>
2022-10-02 10:40:45 +0530

    Added Binary Search in C++ and Python

---
+++ Searching/Binary-Search/in_Python.py
@@ -0,0 +1,24 @@
+def binarySearch(arr, l, r, x):
+    if r >= l:
+ 
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid-1, x)
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+ 
+    else:
+        return -1
+ 
+
+arr = [1,2,4,8,16,32,64]
+x = 32
+ 
+result = binarySearch(arr, 0, len(arr)-1, x)
+ 
+if result != -1:
+    print("Element is present at index % d" % result)
+else:
+    print("Element is not present in array")
