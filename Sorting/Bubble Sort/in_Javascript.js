59733ebeed9b52b0baad2a3c7b1d51f6eb8b420d
rahmat-st <rr7465457@gmail.com>
2022-10-04 01:02:48 +0700

    add: bubble sort explanation and complexity

--- Sorting/Bubble Sort/in_Javascript.js
+++ Sorting/Bubble Sort/in_Javascript.js
@@ -1,34 +1,44 @@
+/**
+ * Explanation:
+ * 		Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping
+ * 		the adjacent elements if they are in the wrong order.
+ *
+ * Time complexity:
+ * 		- Worst and Average Case: O(N2). The worst case occurs when an array is reverse sorted.
+ * 		- Best Case: O(N). The best case occurs when an array is already sorted
+ */
+
 const bubbleSort = (data, type = 'asc') => {
-  const currentList = [...data]
+	const currentList = [...data]
 
-  for (let i = 0; i < currentList.length; ++i) {
-    for (let j = i + 1; j < currentList.length; ++j) {
-      switch (type) {
-        case 'asc':
-          if (currentList[i] > currentList[j]) {
-            let temp
-            temp = currentList[i]
-            currentList[i] = currentList[j]
-            currentList[j] = temp
-          }
-          break
+	for (let i = 0; i < currentList.length; ++i) {
+		for (let j = i + 1; j < currentList.length; ++j) {
+			switch (type) {
+				case 'asc':
+					if (currentList[i] > currentList[j]) {
+						let temp
+						temp = currentList[i]
+						currentList[i] = currentList[j]
+						currentList[j] = temp
+					}
+					break
 
-        case 'desc':
-          if (currentList[i] < currentList[j]) {
-            let temp
-            temp = currentList[i]
-            currentList[i] = currentList[j]
-            currentList[j] = temp
-          }
-          break
+				case 'desc':
+					if (currentList[i] < currentList[j]) {
+						let temp
+						temp = currentList[i]
+						currentList[i] = currentList[j]
+						currentList[j] = temp
+					}
+					break
 
-        default:
-          return 'Type not valid'
-      }
-    }
-  }
+				default:
+					return 'Type not valid'
+			}
+		}
+	}
 
-  return currentList
+	return currentList
 }
 
 /* Example */
