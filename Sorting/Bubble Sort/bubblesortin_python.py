d8d5da3857cbe6053bc6348b5e33917d6b387d36
Fahad Umar Maheri <58872156+fahadmaheri@users.noreply.github.com>
2022-10-09 21:57:00 +0500

    Update and rename bubblesort.py to bubblesortin_python.py
    Renamed and added comments .

--- Sorting/Bubble Sort/bubblesortin_python.py
+++ Sorting/Bubble Sort/bubblesortin_python.py
@@ -1,5 +1,6 @@
 def bubblesort(elements):
 	swapped = False
+	 # Looping from size of array from last index[-1] to index [0]
 	for n in range(len(elements)-1, 0, -1):
 		for i in range(n):
 			if elements[i] > elements[i + 1]:
@@ -7,7 +8,7 @@ def bubblesort(elements):
 				# swapping data if the element is less than next element
 				elements[i], elements[i + 1] = elements[i + 1], elements[i]	
 		if not swapped:
-			# exiting the function if we didn't make a single swap
+			# exiting the function if we didn't make a single swap. meaning that the array is already sorted.
 			return
 
 elements = [39, 12, 18, 85, 72, 10, 2, 18]
