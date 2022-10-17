6643862424de3d65db925cf9459046edffce4a73
Rohan Saini <63922227+Rohan-Saini7996@users.noreply.github.com>
2022-10-17 12:46:15 +0530

    Update in_Java.java

--- Sorting/Bubble Sort/in_Java.java
+++ Sorting/Bubble Sort/in_Java.java
@@ -6,28 +6,17 @@ public class in_Java {
     }
     
     static void bubble(int arr[]){
-        boolean swapped;
-        //runs steps n-1 times
-        for(int i=0;i<arr.length-1;++i){
-            swapped=false;
-            for(int j=1;j<arr.length-i;++j){
- 
-             if(arr[j]<arr[j-1]){
-             //then swap
-             int temp;
-             temp=arr[j];
-             arr[j]=arr[j-1];
-             arr[j-1]=temp;
-            swapped=true;
- 
-             }
-            }
-            //if did not swap for a particular value if i, it means the array is sorted hence stop the program
-            if(swapped==false){
-                break;
-            }
-            //without this if condition it would be of  o(n^2),but with this condition it becomes o(n).
-        }
+        for(int i=0;i<a.length-1;i++)  
+    {  
+        for (int j=0;j<a.length-1;j++)  
+        {  
+            if(a[j]>a[j+1])  
+            {  
+                int temp = a[j];  
+                a[j]=a[j+1];  
+                a[j+1] = temp;   
+            }  
+        }  
     }
 
     
