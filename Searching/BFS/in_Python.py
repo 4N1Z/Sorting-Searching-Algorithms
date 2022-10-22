e0b5546b69d6db0e49dad1389f9363a58186138a
A-nsh-3027 <101976374+A-nsh-3027@users.noreply.github.com>
2022-10-22 09:58:42 +0530

    Python Program for BFS traversal

---
+++ Searching/BFS/in_Python.py
@@ -0,0 +1,58 @@
+# Python3 Program for BFS traversal
+from collections import defaultdict
+
+
+# adjacency list representation
+class Graph:
+
+	# Constructor
+	def __init__(self):
+
+		self.graph = defaultdict(list)
+
+	# add an edge to graph
+	def addEdge(self,u,v):
+		self.graph[u].append(v)
+
+	# print a BFS of graph
+	def BFS(self, s):
+
+		# Mark all the vertices as not visited
+		visited = [False] * (len(self.graph))
+
+		# queue for BFS
+		queue = []
+
+		# Mark the source node as
+		# visited and enqueue it
+		queue.append(s)
+		visited[s] = True
+
+		while queue:
+
+			# Dequeue a vertex from
+			# queue and print it
+			s = queue.pop(0)
+			print (s, end = " ")
+
+			# Get all adjacent vertices of the
+			# dequeued vertex s. If a adjacent
+			# has not been visited, then mark it
+			# visited and enqueue it
+			for i in self.graph[s]:
+				if visited[i] == False:
+					queue.append(i)
+					visited[i] = True
+
+
+# Create a graph 
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+g.BFS(0)
+
