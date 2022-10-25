da3792c048bde5e84d7ac484094353bd83cc261e
Preet Chahal <cyberrayon@gmail.com>
2022-10-25 11:13:32 +0530

    Update in_Java.java
    sorting agorithm added

--- Sorting/Bubble Sort/in_Java.java
+++ Sorting/Bubble Sort/in_Java.java
@@ -2,33 +2,41 @@
 public class in_Java {
 
     public static void main(String[] args) {
-        
-    }
-    
-    static void bubble(int arr[]){
-        boolean swapped;
-        //runs steps n-1 times
-        for(int i=0;i<arr.length-1;++i){
-            swapped=false;
-            for(int j=1;j<arr.length-i;++j){
- 
-             if(arr[j]<arr[j-1]){
-             //then swap
-             int temp;
-             temp=arr[j];
-             arr[j]=arr[j-1];
-             arr[j-1]=temp;
-            swapped=true;
- 
-             }
+      //Declare array
+        int[] arr = {5, 4, 3, 2, 1, 3, 4, 5, 6, 7, 8, 9, 0};
+//Printing ARRAY Before Sorting
+        System.out.println("Printing ARRAY Before Sorting");
+        for (int i : arr
+        ) {
+            System.out.print(i + ",");
+
+        }
+        System.out.println("");
+//Main Logic
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+//                    System.out.println("swapped");
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+
+
             }
-            //if did not swap for a particular value if i, it means the array is sorted hence stop the program
-            if(swapped==false){
-                break;
-            }
-            //without this if condition it would be of  o(n^2),but with this condition it becomes o(n).
+
+        }
+//Printing ARRAY After Sorting
+        System.out.println("Printing ARRAY After Sorting");
+        for (int i : arr
+        ) {
+            System.out.print(i + ",");
+
         }
     }
+    
+  
 
     
 }
